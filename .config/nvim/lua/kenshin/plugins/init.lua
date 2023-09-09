5f61be070b98e85c4ec298f69662dd72ee703b56
Nicolas Sedres <nicolas.sedres.moreira@gmail.com>
2023-09-09 14:05:46 -0300

    jacoco plugin deleted because is not working

--- .config/nvim/lua/kenshin/plugins/init.lua
+++ .config/nvim/lua/kenshin/plugins/init.lua
@@ -2,9 +2,6 @@ return {
   "nvim-lua/popup.nvim",  -- An implementation of the Popup API from vim in Neovim
   "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
   ------------------- SamuPlugins----------------------------------------------------
-  --codeCoverage for jacoco
-  "dsych/blanket.nvim",
   -- tmux & split window navigation
   "christoomey/vim-tmux-navigator",
-  --COLORPICKER
 }
