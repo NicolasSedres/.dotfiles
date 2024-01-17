5a7bd33ef92da4c7e7163fa0e9516f56da31fc39
Samu <nicolas.sedres@nubank.com.br>
2024-01-17 14:39:29 -0300

    sketchybar script and config added to mac

---
+++ mac/scripts/bar.sh
@@ -0,0 +1,11 @@
+brew tap FelixKratz/formulae
+brew tap homebrew/cask-fonts
+brew install font-sf-pro
+brew install --cask sf-symbols
+brew install sketchybar
+curl -L https://github.com/kvndrsslr/sketchybar-app-font/releases/download/v1.0.16/sketchybar-app-font.ttf -o $HOME/Library/Fonts/sketchybar-app-font.ttf
+ln -s ~/.dotfiles/mac/sketchybar sketchybar
+chmod +x ~/.config/sketchybar/plugins/*
+chmod +x ~/.config/sketchybar/items/*
+chmod +x ~/.config/sketchybar/colors.sh
+brew services start sketchybar
---
+++ mac/sketchybar/colors.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export WHITE=0xffE0CDA5
+# -- Samu Scheme --
+export BAR_BORDER=0xff47423E
+export BAR_COLOR=0xff000000
+export ITEM_BG_COLOR=0xffDB4740
+export ACCENT_COLOR=0xffDB4740
+export BRGREEN=0xffB1B946
